6640be7648cce932703d29272f44371526905adf
jkalebe <jeremiaskalebe@gmail.com>
2020-05-02 03:07:14 +0000

    inputs addeted

--- src/openProject.js
+++ src/openProject.js
@@ -1,46 +1,24 @@
-const extractMedia = (xml) => xml.getElementsByTagName("MEDIA_DESCRIPTOR");
-const extractURL = (line) => line.getAttribute('MEDIA_URL');
-const extractDescriptor = (line) => line.getAttribute('RELATIVE_MEDIA_URL');
-const isAudio = (line) => line.getAttribute('MIME_TYPE').search("audio") != -1;
-const isVideo = (line) => line.getAttribute('MIME_TYPE').search("video") != -1;
-const extractParticipant = (xml) => [].map.call( xml.getElementsByTagName('TIER'), function(value){
-  console.log(value.getAttribute('PARTICIPANT') && value.getAttribute('LINGUISTIC_TYPE_REF')=='Phrases' ?  value.getAttribute('PARTICIPANT'):'')
-
-});
-
-const extractPhrasesParticipant = (xml) => 
-  Array.of(xml)
-  .forEach(function(){
-    var obj={};
-    var array=[];
-    for (const item of arguments[0]) {
+function function_open_project(){ 
+  var file = document.getElementById('open_here');
+  var reader = new FileReader();
+  reader.onloadend = function() {
+    if(file.files.length == 1){
       
-      if(item.getAttribute('PARTICIPANT') != null && item.getAttribute('LINGUISTIC_TYPE_REF') == 'Phrases'){
-        obj[item.getAttribute('PARTICIPANT')] =[]
-        array.push(item.getElementsByTagName('ANNOTATION_VALUE')[0].innerHTML);
-        // Array.prototype.push.apply(array, [item.getElementsByTagName('ANNOTATION_VALUE')[0].innerHTML])
-        obj[item.getAttribute('PARTICIPANT')] = array//Array.prototype.push.apply({[item.getAttribute('LINGUISTIC_TYPE_REF')] : item.getElementsByTagName('ANNOTATION_VALUE')[0].innerHTML})
-      }
+      getXmlFile(reader.result, (xml)=> {
+        //new ExtractXml(xml);
+        new CreateElements(xml)
+      })
+      
+      new SubmitDatasFrame()
+      
     }
-    console.log(obj)
-  });
-  
-  
-  function function_open_project(){ 
-    var file = document.getElementById('open_here');
-    var reader = new FileReader();
-    reader.onloadend = function() {
-      if(file.files.length == 1){
-        getXmlFile(reader.result, extractXML);
-  
-}
-}
-reader.readAsDataURL(file.files[0])
+  }
+  reader.readAsDataURL(file.files[0])
 }
 
 function getXmlFile(path, callback) {
   let request = new XMLHttpRequest();
-  var object={};
+  
   request.open("GET", path);
   request.setRequestHeader("Content-Type", "text/xml");
   request.onreadystatechange = function(){
@@ -48,102 +26,139 @@ function getXmlFile(path, callback) {
       callback(request.responseXML);
     }
   };
+  
   request.send();
   
 }
 
-const extractXML = function(xml){
-          
-  const object = {};
-  var urlVideo, urlAudio;
-  var media_video, media_audio;
-  var time_slot_id=[], time_value=[]
+class ExtractXml{
+  constructor(xml){
+    this.xml = xml
+    this.object = {}
+    this.extractDatasOfXML();
+  }
   
-  var participant, type_ref, tier_id, parent_ref;
-  //extractParticipant(xml)
-  
-  extractPhrasesParticipant(xml.getElementsByTagName('TIER'))
-  
-  for (const element of extractMedia(xml)){
-    if(isVideo(element)){
-      urlVideo = extractURL(element)
-      media_video = extractDescriptor(element)
+  extractDatasOfXML(){
+    for (const element of this.extractMedia(this.xml)){
+      if(this.isVideo(element)){
+        this.object['urlVideo'] = this.extractURL(element)
+        this.object['media_url_video'] = this.extractDescriptor(element)
+      }
+      if(this.isAudio(element)){
+        this.object['urlAudio'] = this.extractURL(element)
+        this.object['media_url_audio'] = this.extractDescriptor(element)
     }
-    if(isAudio(element)){
-    urlAudio = extractURL(element)
-    media_audio = extractDescriptor(element)
+  }
+  
+  this.extractTimeAttributes();
+  this.extractTierPhrases();
+  console.log(this.object)
+  this.extractTIER_ID()
+  
+  
+  }
+  
+  extractMedia = () => this.xml.getElementsByTagName("MEDIA_DESCRIPTOR");
+  
+  extractURL = (line) => line.getAttribute('MEDIA_URL');
+  
+  extractDescriptor = (line) => line.getAttribute('RELATIVE_MEDIA_URL').replace("./", "");
+  
+  extractTimeAttributes = () => {
+    let time_slot_id = [], time_value = [];
+    for (const i of this.xml.getElementsByTagName("TIME_SLOT")) {
+      time_slot_id.push(i.getAttribute('TIME_SLOT_ID'))
+      time_value.push(i.getAttribute('TIME_VALUE'))              
+    }
+    this.object['time_slot_id'] = time_slot_id;
+    this.object['time_value'] = time_value;
+  }
+  
+  isAudio = (line) => line.getAttribute('MIME_TYPE').search("audio") != -1;
+  
+  isVideo = (line) => line.getAttribute('MIME_TYPE').search("video") != -1;
+
+  extractTierPhrases = () =>{
+    let phrases = []
+    for(const i of this.xml.getElementsByTagName('TIER')){
+      if(i.getAttribute('LINGUISTIC_TYPE_REF'))
+        phrases.push(i)
+    }
+    this.object['phrases'] = phrases
+  }
+
+  extractTIER_ID(){
+    let array=[]
+    for (const i of this.xml.getElementsByTagName('TIER')) {
+      array.push(i.getAttribute('TIER_ID'))
+    }
+    this.object['TIER_ID'] = array;
   }
   
 }
-object['urlVideo']=urlVideo;
-object['urlAudio']=urlAudio;
-object['media_url_video']=media_video.replace("./", "");
-object['media_url_audio']=media_audio.replace("./", "");
 
-
-//extrai as marcações dos tempos
-for (const i of xml.getElementsByTagName("TIME_SLOT")) {
-  time_slot_id.push(i.getAttribute('TIME_SLOT_ID'))
-  time_value.push(i.getAttribute('TIME_VALUE'))              
-}
-object['time_slot_id']=time_slot_id;
-object['time_value']=time_value;
-
-
-//extrai as trilhas
-var trilhas=xml.getElementsByTagName("TIER");   
-for (const i of trilhas) {
-  participant = i.getAttribute('PARTICIPANT');
-  for (const j of i.attributes) {
-    switch(String(j.name)){
-      case "LINGUISTIC_TYPE_REF":
-        type_ref=j.value;
-        break;
-        
-      case "TIER_ID":
-        tier_id=j.value;
-        
-        break;
-      case "PARENT_REF":
-        parent_ref=j.value;
-
-        break;
-  } 
-}
-//object[participant]={}
-if(type_ref == "Phrases"){
-  var annotation = i.getElementsByTagName("ANNOTATION_VALUE");
-
-  object[participant]={'Phrases': []}
-  for (const phrases_annotation of annotation) {
-    object[participant][type_ref].push(phrases_annotation.innerHTML);
-    console.log("Participant: "+ i.getAttribute('PARTICIPANT') + " !" +  phrases_annotation.innerHTML)
+class SubmitDatasFrame{
+  constructor(){
+    this.input = null
+    this.inputDatasTrilhas()
   }
-}
-if(type_ref=="Words"){
-  var annotationWords = i.getElementsByTagName("ANNOTATION_VALUE")
-  object[participant]={'Words': []}
-  for (const words_annotation of annotationWords) {
-    object[participant][type_ref].push(words_annotation.innerHTML);
+
+  getFrame(){
+    return document.querySelector('[wm-frame=trilhas]')
+  }
+
+  getDocument(){
+    let D = this.getFrame();
+    return (D.contentWindow || D.contentDocument).document;
+  }
+
+  setInputs(){
+    this.input = this.getDocument().querySelectorAll('[wm-inpt]')
+  }
+
+  inputDatasTrilhas(){
+    this.setInputs()
+    this.input.forEach(ipt =>{
+      console.log(ipt.getAttribute('wm-inpt'))
+    })
+    console.log(this.getDocument())
   }
 }
 
-if(type_ref=="Note" && tier_id.search("Translation") != -1){
-  var annotationNote = i.getElementsByTagName("ANNOTATION_VALUE")
-  for (const note_annotation of annotationNote) {
-    
+class CreateElements{
+  constructor(xml){
+    this.frame=document.querySelector('[wm-frame=trilhas]')
+    this.framDoc = (this.frame.contentWindow || this.frame.contentDocument).document
+    this.ExtractXml = new ExtractXml(xml)
+    this.divs = this.createDivs()
+    this.submitDivsTIER_ID()
+    this.createInputs()
   }
-}
 
-if(type_ref=="Note" && tier_id.search("Participant-note")!=-1){
-  var annotationnoteParticipant = i.getElementsByTagName("ANNOTATION_VALUE")
-  for (const noteParticipant_annotation of annotationnoteParticipant) {
-    
+  createDivs(){
+    let  elements  = [""]
+    for (const item of this.ExtractXml.object['TIER_ID']) {
+      elements.push(`<div wm-input=${String(item)} type="text"></div>\n`)
+    }
+    return elements.join("");
   }
-}
-}     
-  
 
-console.log(object)
-//return object;
+  submitDivsTIER_ID(){
+    const D = this.framDoc.querySelector('[wm-div]')
+    D.innerHTML = this.divs
+  }
+
+  createInputs(){
+    let elements = [""]
+    this.ExtractXml.object['phrases'].forEach(v => {
+       for (const i of v.getElementsByTagName('ANNOTATION_VALUE')) {
+        this.framDoc.querySelector(`[wm-input=${v.getAttribute('TIER_ID')}]`).innerHTML +=`<input value="${i.innerHTML}" type="text">`
+       }
+    })
+    //console.log(elements.join('\n'))
+    return elements.join('\n')
+  }
+
+
+
 }
