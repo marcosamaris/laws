818f38b4902afc73868cf0d9070865e7bac82ed0
jkalebe <jeremiaskalebe@gmail.com>
2020-07-28 02:19:25 +0000

    new pages

--- jsx/App/App.jsx
+++ jsx/App/App.jsx
@@ -4,6 +4,10 @@ import { Route } from 'react-router-dom';
 import { LandingPage } from './LandingPage.jsx';
 import { StoryIndex } from './StoryIndex.jsx';
 import { Stories } from './Stories/Stories.jsx';
+import { OpenProject } from './OpenProject.jsx';
+import { NewProject } from './NewProject.jsx';
+import { Home } from './Home.jsx';
+import { About } from './About.jsx';
 
 export function App({ data }) {
     return (
@@ -11,6 +15,10 @@ export function App({ data }) {
             <Route exact path="/" render={props => <LandingPage/>} />
             <Route exact path="/index" render={props => <StoryIndex index={data.index} />} />
             <Route path="/story" render={props => <Stories stories={data.stories} />} />
+            <Route exact path="/openproject" render={props => <OpenProject/>}/>
+            <Route exact path="/newproject" render={props => <NewProject/>}/>
+            <Route exact path="/about" render={props => <About/>}/>
+            <Route exact path="/home" render={props => <Home/>}/>
         </div>
     );
 }
